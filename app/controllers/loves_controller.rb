f1718c31c8e680774c332658a157dc33fd15a281
Omar Vargas <ovargas27@gmail.com>
2011-06-28 23:10:12 -0500

    Enable love helper in love controller

--- app/controllers/loves_controller.rb
+++ app/controllers/loves_controller.rb
@@ -1,4 +1,5 @@
 class LovesController < ActionController::Base
+  helper :love
   
   def create
     @love = Love.create( :member_id => current_member.id, :loveable_id => params[:loveable_id], :loveable_type => params[:loveable_type]) unless current_member.nil?
