a9c1e024577db8278c9d727a2cdd9bf4f7d3f159
Mariana Hernández <marianahj@gmail.com>
2011-07-05 18:26:44 -0500

    fix typo

--- app/helpers/worn_helper.rb
+++ app/helpers/worn_helper.rb
@@ -2,19 +2,19 @@ module WornHelper
   def worn_button(obj)
     if member_signed_in?
       if obj.worn_by?(current_member)
-        link = link_to "#{obj.worn.count} UNWORN", worn_path(:wornable_id => obj.id, :wornable_type => obj.class.name), :method => :delete
+        link = link_to "#{obj.worns.count} UNWORN", worn_path(:wornable_id => obj.id, :wornable_type => obj.class.name), :method => :delete
       else
-        link = content_tag(:div, "#{obj.worn.count} WORN".html_safe + get_kinds_list(obj),{:class => "worns"})
+        link = content_tag(:div, "#{obj.worns.count} WORN".html_safe + get_kinds_list(obj),{:class => "worns"})
       end
     else
-      link = link_to "#{obj.worn.count} WORN", new_member_session_path
+      link = link_to "#{obj.worns.count} WORN", new_member_session_path
     end
     count = obj.worn.count
     content_tag(:div, link, {:id => 'worns'})
   end
 
   def get_kinds_list(obj)
-    content_tag(:ul, :class => "worn") { ["day", "work", "evening"].each do |kind|
+    content_tag(:ul, :class => "worns") { ["day", "work", "evening"].each do |kind|
       li = link_to kind, worn_path(:wornable_id => obj.id,
                                    :wornable_type => obj.class.name,
                                    :kind => kind), :remote => true, :method => :post
@@ -23,6 +23,6 @@ module WornHelper
   end
 
   def worn_counter(obj)
-    content_tag(:span, "#{obj.worn.count} WORN", :class => 'wornCount')
+    content_tag(:span, "#{obj.worns.count} WORN", :class => 'wornCount')
   end
 end
