96582c2f24c2e3e4df21ca4a13eb95ee04b8dc44
Carlos Muñiz <carlos.muniz@crowdint.com>
2011-07-07 20:06:04 -0500

    fix manual filter product search

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,23 +7,28 @@ module Admin
             :title_attribute => 'name', :xhr_paging => true
 
     def index
-      unless params[:search].blank?
-        query_products= {:name_or_description_or_category_name_or_sub_category_name_contains => params[:search] }
-        products = Product.search(query_products)
-        @products = products.relation
+      unless params[:q].blank?
+        @products = Product.where('title like ?', "%#{params[:q]}%").select("title,id")
+        render :json => @products.map! { |product| {"id" => product.id, "name" => product.title} }
       else
-        per_page = params[:per_page].present? ? params[:per_page].to_i : 20
-        params[:page] ||= 1
-        @products = Product
-        @products = case params[:order]
-                   when 'loved'
-                     @products.order('loves_count DESC').paginate(:page => params[:page], :per_page => per_page)
-                   when 'commented'
-                     @products.order('comments_count DESC').paginate(:page => params[:page], :per_page => per_page)
-                   else
-                     @products.paginate(:page => params[:page], :per_page => per_page)
-                   end
-        render :partial => 'products' if request.xhr?
+        unless params[:search].blank?
+          query_products= {:name_or_description_or_category_name_or_sub_category_name_contains => params[:search]}
+          products = Product.search(query_products)
+          @products = products.relation
+        else
+          per_page = params[:per_page].present? ? params[:per_page].to_i : 20
+          params[:page] ||= 1
+          @products = Product
+          @products = case params[:order]
+            when 'loved'
+              @products.order('loves_count DESC').paginate(:page => params[:page], :per_page => per_page)
+            when 'commented'
+              @products.order('comments_count DESC').paginate(:page => params[:page], :per_page => per_page)
+            else
+              @products.paginate(:page => params[:page], :per_page => per_page)
+                      end
+          render :partial => 'products' if request.xhr?
+        end
       end
     end
 
