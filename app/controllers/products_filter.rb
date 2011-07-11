6d2577b70259925e45e91ac970ee2d8a65e29d01
Mario Alberto Chavez <mario.chavez@gmail.com>
2011-07-11 12:43:38 -0500

    Moved manual filter and ordering off products_filter to resources_filter in main proyect

--- app/controllers/products_filter.rb
+++ app/controllers/products_filter.rb
@@ -1,15 +1,4 @@
 module ProductsFilter
-  def sort_products!
-    order = 'created_at DESC'
-    if params[:order]
-      order = 'loves_count DESC' if params[:order] == 'loved'
-      order = 'worns_count DESC' if params[:order] == 'worns'
-      order = 'price DESC' if params[:order] == 'highest-price'
-      order = 'price ASC' if params[:order] == 'lowest-price'
-
-    end
-    @products = @products.nil? ? Product.order(order) : @products.order(order)
-  end
 
   def filter_by_brand!
     @products = @products.by_brand(params[:brand_name]) if params[:brand_name]
@@ -19,13 +8,4 @@ module ProductsFilter
     @products = @products.by_category(params[:category_name]) if params[:category_name]
   end
 
-  def apply_manual_filters!
-    if params[:filter]
-      manual_filter = ManualFilter.where(:name => params[:filter]).first
-      if manual_filter
-        products_id = manual_filter.product_sources.select('products.id').map{|p| p.id}
-        @products = @products.where(:id => products_id)
-      end
-    end
-  end
 end
