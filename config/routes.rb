e2c6c45bec0c8345b53c4c7e7abfd827fc58501c
Mario Alberto Chavez <mario.chavez@gmail.com>
2011-07-12 01:22:39 -0500

    Removed unused routes and changed the way to build brand menu

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@
   resource :love, :only => [:create, :destroy]
   resource :worn, :only => [:create, :destroy]
   
-  #match 'products/brand/:brand_name' => 'products#index'
-  #match 'products/category/:category_name' => 'products#index'
-  #match "products/filter/:filter" => "products#index"
-
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
     resources :products, :except => :show do
       collection do
